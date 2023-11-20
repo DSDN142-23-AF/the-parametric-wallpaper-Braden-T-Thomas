9311f111cc46c8713521ff5fa1b402088f508597
Braden-T-Thomas <braden.t.thomas@gmail.com>
2023-11-20 14:15:53 +1300

    Update my_wallpaper.js

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,13 +1,20 @@
 //your parameter variables go here!
-let top_eyelid = 75 //Default Val = 75
-let bottom_eyelid = 125 //Default Val = 125
-let oval_eye = true //Default Val = false
+//Use to change how open / closed the eye is. Changing the left eyelid will change the right to be equal
+let left_eyelid = 85 // Default Val = 85
+let right_eyelid = 200 - left_eyelid
+
+let left_eye_top = 35 // Default val = 35
+let right_eye_top = 200 - left_eye_top
+let left_eye_bottom = 75 // Default val = 75
+let right_eye_bottom = 200 - left_eye_bottom
+
+let oval_eye = false //Default Val = false
 
 
 function setup_wallpaper(pWallpaper) {
   pWallpaper.output_mode(DEVELOP_GLYPH);
   pWallpaper.resolution(FIT_TO_SCREEN);
-  pWallpaper.show_guide(true); //set this to false when you're ready to print
+  pWallpaper.show_guide(false); //set this to false when you're ready to print
 
   //Grid settings
   pWallpaper.grid_settings.cell_width  = 200;
@@ -25,33 +32,19 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
   line(0, 100, 200, 100);
   line(100, 0, 100, 200);
 
-  /**Eye point guides
+  //Eye point guides
+  /**
   strokeWeight(3);
-  point(25, 100);
-  point(100, 75);
-  point(175, 100);
+  point(165, 35);
+  point(125, 85);
+  point(155, 67);
+  point(135, 50);
   */
 
   if (!oval_eye){
     //Top eyelid
-  noFill();
-  strokeWeight(1.5);
-  beginShape();
-  curveVertex(25, 100);
-  curveVertex(25, 100);
-  curveVertex(100, top_eyelid);
-  curveVertex(175, 100);
-  curveVertex(175, 100);
-  endShape();
-
-  //Bottom eyelid
-  beginShape();
-  curveVertex(25, 100);
-  curveVertex(25, 100);
-  curveVertex(100, bottom_eyelid);
-  curveVertex(175, 100);
-  curveVertex(175, 100);
-  endShape();
+    normal_eye_draw();
+    
   }
   else{
     let c = color('#f1d027');
@@ -67,4 +60,62 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
     line(100, 102, 100, 98);
   }
 
+}
+
+function normal_eye_draw(){
+  // Main middle eye
+  //Left eyelid
+  strokeWeight(1.5);
+  beginShape();
+  curveVertex(100, 10);
+  curveVertex(100, 10);
+  curveVertex(left_eyelid, 55);
+  curveVertex(100, 100);
+  curveVertex(100, 100);
+  endShape();
+
+  //Right eyelid
+  beginShape();
+  curveVertex(100, 10);
+  curveVertex(100, 10);
+  curveVertex(right_eyelid, 55);
+  curveVertex(100, 100);
+  curveVertex(100, 100);
+  endShape();
+
+
+  // Flanking eyes
+  // Right eye
+  beginShape();
+  curveVertex(right_eye_top, 35);
+  curveVertex(right_eye_top, 35);
+  curveVertex(135, 50);
+  curveVertex(right_eye_bottom, 85);
+  curveVertex(right_eye_bottom, 85);
+  endShape();
+
+  beginShape();
+  curveVertex(right_eye_top, 35);
+  curveVertex(right_eye_top, 35);
+  curveVertex(155, 67);
+  curveVertex(right_eye_bottom, 85);
+  curveVertex(right_eye_bottom, 85);
+  endShape();
+
+  // Left eye
+  beginShape();
+  curveVertex(left_eye_top, 35);
+  curveVertex(left_eye_top, 35);
+  curveVertex(65, 50);
+  curveVertex(left_eye_bottom, 85);
+  curveVertex(left_eye_bottom, 85);
+  endShape();
+
+  beginShape();
+  curveVertex(left_eye_top, 35);
+  curveVertex(left_eye_top, 35);
+  curveVertex(45, 67);
+  curveVertex(left_eye_bottom, 85);
+  curveVertex(left_eye_bottom, 85);
+  endShape();
 }
